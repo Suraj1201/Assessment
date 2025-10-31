611f22cd61fd26a695da7d27ad50ddd2f7a4e74c
Dhruvi12-2004 <P2838561@my365.dmu.ac.uk>
2025-10-31 17:40:49 +0000

    Added AccountController actions:Orders,Profile

--- CyberSpaceGamers/Controllers/AccountController.cs
+++ CyberSpaceGamers/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using CyberSpaceGamers.Models.ViewModels;
-using System.Collections.Generic;
 
 namespace CyberSpaceGamers.Controllers
 {
@@ -18,59 +16,24 @@ namespace CyberSpaceGamers.Controllers
             return View();
         }
 
-        // GET: /Account/Dashboard
-        public IActionResult Dashboard()
+        // /Account
+        public IActionResult Index()
         {
-            var vm = new AccountDashboardViewModel
-            {
-                Tiles = new List<DashboardTile>
-                {
-                    new DashboardTile
-                    {
-                        Title = "Profile",
-                        Description = "View and edit your profile",
-                        Icon = "bi-person",
-                        Controller = "Account",
-                        Action = "Profile"
-                    },
-                    new DashboardTile
-                    {
-                        Title = "Orders",
-                        Description = "See your order history",
-                        Icon = "bi-receipt",
-                        Controller = "Account",
-                        Action = "Orders"
-                    },
-                    new DashboardTile
-                    {
-                        Title = "Wishlist",
-                        Description = "Saved games to buy later",
-                        Icon = "bi-heart",
-                        Controller = "Product",
-                        Action = "Index"   // placeholder; change to Wishlist later
-                    }
-                }
-            };
-
-            return View(vm);
+            return View(); // will render Views/Account/Index.cshtml
         }
 
         // GET: /Account/Orders
         public IActionResult Orders()
         {
-            return View(); // view-only placeholder
-        }
-
-        // GET: /Account/Settings
-        public IActionResult Wishlist()
-        {
-            return View(); // view-only placeholder
+            // simple view-only page for now
+            return View();
         }
 
         // GET: /Account/Profile
         public IActionResult Profile()
         {
-            return View(); // view-only placeholder
+            // simple view-only page for now
+            return View();
         }
     }
 }
